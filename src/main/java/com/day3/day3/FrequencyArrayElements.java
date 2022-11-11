a6dcfb32190654354c3fced5d731ddd4c8ab8972
Kunal <kunalsonawane730@gmail.com>
2022-11-12 02:17:40 +0530

    Adding Frequency of array elements program

---
+++ src/main/java/com/day3/day3/FrequencyArrayElements.java
@@ -0,0 +1,25 @@
+package com.day3.day3;
+
+public class FrequencyArrayElements {
+
+	public static void main(String[] args) {
+		   int[] a = {10, 50, 10, 60, 40, 70, 60};
+
+	        for (int i=0; i<a.length; i++) {
+	            int x = a[i];
+	            int count = 0;
+
+	            if (x == -1)continue;
+
+	            for (int j=0; j<a.length; j++) {
+
+	                if (a[j] == x) {
+	                    count++;
+	                    a[j] = -1;
+	                }
+	            }
+	            System.out.println("Frequency of " +x+ " is " +count);
+	        }
+	}
+
+}
